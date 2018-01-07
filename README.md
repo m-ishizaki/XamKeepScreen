04e83d00d4391d0880e936be3b79224ed8646fe7
m ishizaki <love.ms.is12t@hotmail.co.jp>
2018-01-08 02:41:10 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # XamKeepScreen
+Xamarin Forms で画面のスリープ/自動ロックをオフにします。  
+# 使い方
+C# での使用例
+```cs
+RKSoftware.XamKeepScreen.KeepScreen.On();
+// ...時間のかかる処理...
+RKSoftware.XamKeepScreen.KeepScreen.Off();
+```
+# プラットフォーム
+対応プラットフォーム  
+- Android  
+- iOS  
---
+++ src/RKSoftware.XamKeepScreen/RKSoftware.XamKeepScreen/KeepScreen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RKSoftware.XamKeepScreen
+{
+    public static class KeepScreen
+    {
+        public static void Off() => Set(false);
+        public static void On() => Set(true);
+
+        public static void Set(bool isOn)
+        {
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android: SetForAndroid(isOn); break;
+                case Device.iOS: SetForIOS(isOn); break;
+                default: throw new NotSupportedException();
+            }
+        }
+
+        private static bool SetForAndroid(bool isOn)
+        {
+            var formsType = Type.GetType("Xamarin.Forms.Forms,Xamarin.Forms.Platform.Android");
+
+            var activityType = Type.GetType("Android.App.Activity,Mono.Android");
+            var windowType = Type.GetType("Android.Views.Window,Mono.Android");
+            var windowManagerFlagsType = Type.GetType("Android.Views.WindowManagerFlags,Mono.Android");
+
+            var method = windowType.GetMethod(isOn ? "AddFlags" : "ClearFlags");
+            var activityInstance = formsType.GetProperty("Context").GetValue(null);
+            var windowInstance = activityType.GetProperty("Window").GetValue(activityInstance);
+            var keepScreenOnValue = Enum.Parse(windowManagerFlagsType, "KeepScreenOn");
+
+            Device.BeginInvokeOnMainThread(() =>
+                method.Invoke(windowInstance, new[] { keepScreenOnValue })
+            );
+
+            return true;
+        }
+
+        private static bool SetForIOS(bool isOn)
+        {
+            var uiapplicationType = Type.GetType("UIKit.UIApplication,Xamarin.iOS");
+
+            var property = uiapplicationType.GetProperty("IdleTimerDisabled");
+            var uiapplicationInstance = uiapplicationType.GetProperty("SharedApplication").GetValue(null);
+
+            Device.BeginInvokeOnMainThread(() =>
+                property.SetValue(uiapplicationInstance, isOn)
+            );
+
+            return true;
+        }
+    }
+}
